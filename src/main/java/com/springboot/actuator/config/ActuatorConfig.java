af566bd68d8946eb630075dff6d3379eb65e49af
Eclipse_ <nightsky5070@gmail.com>
2023-08-02 14:19:36 +0900

    info env 수정

---
+++ src/main/java/com/springboot/actuator/config/ActuatorConfig.java
@@ -0,0 +1,16 @@
+package com.springboot.actuator.config;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ActuatorConfig {
+
+//    @Bean
+//    public Info info(){
+//        return new info();
+//    }
+
+}
